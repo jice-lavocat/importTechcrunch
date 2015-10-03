791f56eda60dba2ec4898a865994178f2cdd9197
Jean-Christophe Lavocat <jice@lavocat.name>
2015-10-03 10:19:12 +0200

    Added a cleaning step for html

---
+++ contentProcessing.py
@@ -0,0 +1,12 @@
+import re
+
+
+def html2flat(html):
+	""" Takes html and return clean html where :
+			* <a> have been removed
+	"""
+
+	cleanHtml = html
+	cleanHtml = re.sub('</?[div|a|iframe^>]*>', '', cleanHtml)
+
+	return cleanHtml
